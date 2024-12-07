8b27215b155e38c64e03e9b15b1896c6e8976cb3
alinsucea <alin.sucea29@gmail.com>
2024-12-07 16:55:35 +0200

    l am facut sa mearga

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -37,13 +37,8 @@ function App() {
       <div className="container mx-auto px-4 py-8">
         <header className="text-center mb-12">
           <h1 className="text-4xl font-bold text-primary-light mb-2 flex items-center justify-center gap-2">
-<<<<<<< HEAD
             <img src="cat.jpeg" className="w-8 h-8 text-primary-light" />
             Would You Rather?
-=======
-            <Sparkles className="w-8 h-8 text-primary-light" />
-            What Would You Rather?
->>>>>>> 525a234e95f79b2f1239d5b53ae5051500b61d25
           </h1>
           <p className="text-gray-300">Choose your preferred happy moment!</p>
         </header>
@@ -70,4 +65,4 @@ function App() {
   );
 }
 
-export default App;
+export default App;
